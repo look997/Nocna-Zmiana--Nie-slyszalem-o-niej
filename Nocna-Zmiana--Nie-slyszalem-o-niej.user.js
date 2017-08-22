bd2deaa120323a39baa2bb68fd9cfe22d370276c
look997 <look997@users.noreply.github.com>
2017-08-22 16:53:39 +0200

    Update Nocna-Zmiana--Nie-slyszalem-o-niej.user.js

--- Nocna-Zmiana--Nie-slyszalem-o-niej.user.js
+++ Nocna-Zmiana--Nie-slyszalem-o-niej.user.js
@@ -27,7 +27,7 @@ function main() {
 		const stopHiddingHour = 6; // tylko pełne godziny: 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23
 		
 		// Ukrywanie - włącz/wyłącz:
-		const hideHours = false; // ukrywa wpisy w [ustawionych] godzinach
+		const hideHours = true; // ukrywa wpisy w [ustawionych] godzinach
 		const hideNoTag = true; // ukrywa wpisy bez tagów
 		
 		// Tylko oznaczenie czerwoną ramką - włącz/wyłącz:
