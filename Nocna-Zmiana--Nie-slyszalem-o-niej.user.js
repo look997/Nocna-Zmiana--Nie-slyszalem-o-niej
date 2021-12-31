f2466da8a6e67598f4dfa4752744b8194c2b0b45
look997 <look997@users.noreply.github.com>
2021-12-31 12:26:12 +0100

    Update Nocna-Zmiana--Nie-slyszalem-o-niej.user.js

--- Nocna-Zmiana--Nie-slyszalem-o-niej.user.js
+++ Nocna-Zmiana--Nie-slyszalem-o-niej.user.js
@@ -1,27 +1,32 @@
+// @ts-ignore
+const VSCodeLMDate = "2021/12/31 12:25:42";
+
+// @ts-ignore
+const diffTime = Math.abs(Date.now() - new Date(VSCodeLMDate).getTime())/1000;
+// console.log(VSCodeLMDate, `${Math.floor(diffTime)} secs ago`);
+
 // ==UserScript==
-// @name		Nocna Zmiana? Nie słyszałem o niej.
-// @namespace		https://www.wykop.pl/ludzie/look997/
-// @description		 Ukrywa wszystkie wpisy dodanych od 00:00 do 6:00 w serwisie Wykop.pl, z tzw. "Nocnej zmiany" oraz wpisy bez tagów (wszystko da się ustawić pod siebie).
-// @author		look997
-// @version		0.5 beta
-// @grant		none
-// @include		https://www.wykop.pl/*
-// @date           2020-04-13
-// @resource       metadata 
-// @downloadURL    
-// @updateURL      
-// @run-at 		document-end
+// @name        Nocna Zmiana? Nie słyszałem o niej.
+// @description Ukrywa wszystkie wpisy dodanych od 00:00 do 6:00 w serwisie Wykop.pl, z tzw. "Nocnej zmiany" oraz wpisy bez tagów (wszystko da się ustawić pod siebie).
+// @version     0.6 beta
+// @author      look997
+// @include     https://www.wykop.pl/*
+// @grant       none
+// @require     https://greasyfork.org/scripts/437595-wykopobserve/code/WykopObserve.js?version=1002287
+// @run-at      document-end
+// @homepageURL https://www.wykop.pl/ludzie/addons/look997/
+// @namespace	  https://www.wykop.pl/ludzie/addons/look997/
+// @downloadURL https://github.com/look997/Nocna-Zmiana--Nie-slyszalem-o-niej/raw/master/Nocna-Zmiana--Nie-slyszalem-o-niej.user.js
+// @updateURL   https://github.com/look997/Nocna-Zmiana--Nie-slyszalem-o-niej/raw/master/Nocna-Zmiana--Nie-slyszalem-o-niej.user.js
+// @resource    metadata https://github.com/look997/Nocna-Zmiana--Nie-slyszalem-o-niej/raw/master/Nocna-Zmiana--Nie-slyszalem-o-niej.user.js
+// @icon        https://www.google.com/s2/favicons?domain=wykop.pl
+// @icon64      https://www.google.com/s2/favicons?domain=wykop.pl
 // ==/UserScript==
 
-"use strict";
-
-function main() {
-	"use strict";
+(async function () {
 	
-	
-
-	// USTWIENIA
-	
+	// USTAWIENIA
+		
 		// Godziny ukrywane:
 		const startHiddingHour = 0; // tylko pełne godziny: 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23
 		const stopHiddingHour = 7; // tylko pełne godziny: 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23
@@ -30,163 +35,59 @@ function main() {
 		const hideHours = false; // ukrywa wpisy z [ustawionych] godzin
 		const hideNoTag = false; // ukrywa wpisy bez tagów
   	
-  	const hideInHours = true; // ukrywa nocne/tagowane wpisy POZA wyznaczonymi godzinami (kto siedzi w nocy-widzi, kto siedzi w dzień-nie widzi)
-		
-		// Tylko oznaczenie czerwoną ramką - włącz/wyłącz:
+  	const nightVision = true; // gdy ustawisz true:
+															// siedzisz w dzień - widzisz tylko dzienną
+															// siedzisz w nocy - widzisz I DZIENNĄ, I NOCNĄ
+															// - taka furtka, gdy chcesz rozdzielić dzień od nocy :)
+															
+		// Oznaczenie czerwoną ramką - włącz/wyłącz (wpis z ramką widoczny, gdy hideHours/HideNoTag/nightVision jest na false):
 		const markHours = true; // oznacza czerwoną ramką wpisy w [ustawionych] godzinach
 		const markNoTag = true; // oznacza czerwoną ramką wpisy bez tagów
-	
-	// Koniec USTAWIEŃ
-
-
-	let MutationObserver = window.MutationObserver || window.WebKitMutationObserver || window.MozMutationObserver;
-
-	function appendChild (parentEl, stringEl) {
-		const spanEl = document.createElement("span");
-		spanEl.innerHTML = stringEl;
-		parentEl.appendChild(spanEl.firstElementChild);
-	}
-
-	
-	const allFn = (scopeEl)=> {
-	console.log("allFn 0");
-	
-	const EntryEls = Array.prototype.slice.call(document.querySelectorAll("#itemsStream > .entry"));
-	//console.log(EntryEls);
-	EntryEls.forEach((entryEl)=>{
-    const d = new Date();
-    const n = d.getHours();
-		const entryDate = entryEl.querySelector("time").title;
-		const hour = entryDate.split(" ")[1].split(":")[0];
-		const tagExist = entryEl.querySelector(".text").querySelector(".showTagSummary");
-		console.log(tagExist);
-		//if((!hideNoTag && hour >= startHiddingHour && hour <= stopHiddingHour-1) || (hideNoTag && !tagExist)) {
-		if (hour >= startHiddingHour && hour <= stopHiddingHour-1) {
-			if (hideHours && !location.toString().includes("https://www.wykop.pl/wpis")) {
-				entryEl.style.display = "none";
-			}
-			if (markHours || (hideHours && location.toString().includes("https://www.wykop.pl/wpis"))) {
-				entryEl.style.borderRight = "3px solid #a22a2a";
-				//entryEl.style.backgroundColor = "#412c2c";
-			}
-			console.log(entryEl);
-		}
-		if (!tagExist) {
-			if (hideNoTag && !location.toString().includes("https://www.wykop.pl/wpis")) {
-				entryEl.style.display = "none";
-			}
-			if (markNoTag || (hideNoTag && location.toString().includes("https://www.wykop.pl/wpis"))) {
-				entryEl.style.borderRight = "3px solid #a22a2a";
-				//entryEl.style.backgroundColor = "#412c2c";
-			}
-		}
-    if (hideInHours && !location.toString().includes("https://www.wykop.pl/wpis")) {
-      if ((startHiddingHour > n || stopHiddingHour <= n) && hour >= startHiddingHour && hour <= stopHiddingHour-1) {
-          entryEl.style.display = "none";
-      }
-      if ((startHiddingHour > n || stopHiddingHour <= n) && !tagExist) {
-          entryEl.style.display = "none";
-      }
-    }
-	});
-	
-
-
-}
-allFn(document);
-
-	let subStreamFn = (target)=> {
-		//console.log("wpis allFn", target);
-		let childCount = target.childElementCount;
-		target.setAttribute("data-childCount", childCount);
-		allFn(target);
-	}
-
-	let subObserver = new MutationObserver(function(mutations) {
-		let target = mutations[0].target;
-		subStreamFn(target);
-	});
-
-
-
-	let wpisZeroObserver = new MutationObserver(function(mutations) {
-		let liEl = mutations[0].target;
-		let subEl = liEl.querySelector(".sub");
-		//console.log("MOcommentsLi");
-		if (liEl.childElementCount !== 1 && subEl) {
-			//console.log("MOcommentsLi2");
-			allFn(subEl);
-			let subElChildCount = subEl.childElementCount;
-			subEl.setAttribute("data-childCount", subElChildCount);
-			subObserver.observe( subEl, {childList: true} );
-
-			//console.log("MOcommentsLi3");
-		}
-	});
-
-	let subZeroFn = ()=> {
-		const streamElChildren = Array.prototype.slice.call( document.querySelector(".comments-stream").children );
-
-		streamElChildren.forEach((liEl)=>{
-			//console.log("commentsLi");
-			if (liEl.childElementCount === 1) {
-				//console.log("commentsLi2");
-				wpisZeroObserver.observe( liEl, {childList: true} );
-			}
-		});
-	}
-
-	let streamObserver = new MutationObserver(function(mutations) {
-		//console.log("streamO-1");
-		let target = mutations[0].target;
-		//console.log("streamO0");
-		subStreamFn(target);
 		
-		subZeroFn();
-	});
-
-	const subEls = Array.prototype.slice.call( document.querySelectorAll(".sub") );
-
-	subEls.forEach((subEl)=>{
-		if (!subEl.hasAttribute("data-childCount")) {
-			let subElChildCount = subEl.childElementCount;
-			subEl.setAttribute("data-childCount", subElChildCount);
-			subObserver.observe( subEl, {childList: true} );
+	// Koniec USTAWIEŃ
+	
+	const filter = [
+		"mikroblog-page-comment",
+		"tag-page-comment",
+		"moj-page-comment",
+		"glowna-page-comment",
+		"ludzie-page-wpis-comment",
+		
+	];
+	// @ts-ignore
+	wykopObserve(filter, function ({profileEl, liEl, contentEl}, {place, isFirstTime, nick, authorSex}) {
+		const entryDate = liEl.querySelector("time").title;
+		const postHour = entryDate.split(" ")[1].split(":")[0];
+		
+		const hasTags = liEl.querySelector(".text").querySelector(".showTagSummary");
+		const isNightPost = postHour >= startHiddingHour && postHour <= stopHiddingHour-1;
+		
+		if (isNightPost) {
+			if (hideHours) {
+				liEl.style.display = "none";
+			}
+			if (markHours) {
+				liEl.style.borderRight = "3px solid #a22a2a";
+			}
 		}
-	});
-
-	subZeroFn();
-
-	const wpis = location.pathname.split("/")[1];
-	if (wpis !== "wpis") {
-		//console.log("wpis");
-		const streamEl = document.querySelector(".comments-stream");
-		if (!streamEl.hasAttribute("data-childCount")) {
-			//console.log("stream has child");
-			let streamElChildCount = streamEl.childElementCount;
-			streamEl.setAttribute("data-childCount", streamElChildCount);
-			streamObserver.observe( streamEl, {childList: true} );
-			//console.log("stream has child end");
+		if (!hasTags) {
+			if (hideNoTag) {
+				liEl.style.display = "none";
+			}
+			if (markNoTag) {
+				liEl.style.borderRight = "3px solid #a22a2a";
+			}
 		}
-	}
-}
-
-if (typeof $ == 'undefined') {
-	if (typeof unsafeWindow !== 'undefined' && unsafeWindow.jQuery) {
-		// Firefox
-		var $ = unsafeWindow.jQuery;
-		main();
-	} else {
-		// Chrome
-		addJQuery(main);
-	}
-} else {
-	// Opera >.>
-	main();
-}
-
-function addJQuery(callback) {
-	var script = document.createElement("script");
-	script.textContent = "(" + callback.toString() + ")();";
-	document.body.appendChild(script);
-}
+		if ( nightVision && (isNightPost || !hasTags) ) {
+			const currentHour = new Date().getHours();
+			
+			if ( !(currentHour >= startHiddingHour && currentHour <= stopHiddingHour-1) ) {
+				liEl.style.display = "none";
+			}
+		}
+		
+	}, {once:true});
+	
+	// INIT End
+	
+})()
